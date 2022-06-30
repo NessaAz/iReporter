2b1f98e90881b1a31078b161fdaf5c4e86498968
IanoSang <ianosang18@gmail.com>
2022-06-30 19:46:47 +0300

    feat: app and project- adds all app and project files and directories

--- Back-End/app/admin.py
+++ Back-End/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(RedFlag),
+admin.site.register(Intervention),
+admin.site.register(Status)
--- Back-End/app/models.py
+++ Back-End/app/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class RedFlag(models.Model):
+    image = CloudinaryField()
+    title = models.CharField(max_length=250, blank=True)
+    info = models.CharField(max_length=1050, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='flags')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} Red-flag'
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def save_flag(self):
+        self.save()
+
+    def delete_flag(self):
+        self.delete()
+
+
+class Intervention(models.Model):
+    image = CloudinaryField()
+    title = models.CharField(max_length=250, blank=True)
+    info = models.CharField(max_length=1050, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='interventions')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} intervention'
+
+    class Meta:
+        ordering = ["-pk"]
+
+    def save_intervention(self):
+        self.save()
+
+    def delete_intervention(self):
+        self.delete()
+
+
+class Status(models.Model):
+    status = (
+        ('investigation', 'investigation'),
+        ('rejected', 'rejected'),
+        ('resolved', 'resolved'),
+    )
