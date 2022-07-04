2bd16ada7a4c13fe606e48dbf217cf3978c0a1a6
IanoSang <ianosang18@gmail.com>
2022-07-04 18:06:42 +0300

    deployment files

--- Back-End/iReporter/settings.py
+++ Back-End/iReporter/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # OWN
-    'app.apps.AppConfig',
+    'app',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
