30e9107a0c07683b7c837bc5bdefeeb4f628a4c7
LilianMwaura <wambuimwaura28@gmail.com>
2022-07-05 21:36:26 +0300

    create login and sign-up components

--- Front-End/iReporter/src/app/app.module.ts
+++ Front-End/iReporter/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsComponent } from './components/posts/posts.component';
 import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    LoginComponent,
+    SignupComponent,
+  
   ],
     imports: [
         BrowserModule,
@@ -18,17 +21,6 @@ import {FormsModule} from "@angular/forms";
         FormsModule
     ],
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
   providers: [],
   bootstrap: [AppComponent]
 })
