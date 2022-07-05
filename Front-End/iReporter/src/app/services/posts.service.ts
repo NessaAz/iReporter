2490b359c06592c36e1557677a98a861b03a1d7c
IanoSang <ianosang18@gmail.com>
2022-07-05 13:09:50 +0300

    feat: intervetion services- adds a service to consume intervations api and obtain data from db

--- Front-End/iReporter/src/app/services/posts.service.ts
+++ Front-End/iReporter/src/app/services/posts.service.ts
@@ -12,20 +12,25 @@ export class PostsService {
   getAllRedFlags(): Observable<any>{
     return this.http.get(this.baseurl+'/flaglists', {headers: this.httpHeaders});
   }
+
   getOneRedFlag(id:number): Observable<any>{
     return this.http.get(this.baseurl+'/raiseflag/' + id + '/', {headers: this.httpHeaders});
   }
+
   updateRedFlag(redflag:any): Observable<any>{
     const body = {title: redflag.title, info:redflag.info, location:redflag.location }
     return this.http.put(this.baseurl+'/raiseflag/' + redflag.id + '/', body,
       {headers: this.httpHeaders});
   }
+
   createRedFlag(redflag:any): Observable<any>{
     const body = {image: redflag.image, title: redflag.title, info:redflag.info, location:redflag.location }
     return this.http.post(this.baseurl+'/flaglists', body,{headers: this.httpHeaders});
   }
+
   deleteRedFlag(id:any): Observable<any> {
     return this.http.delete(this.baseurl + '/raiseflag/' + id + '/',
       {headers: this.httpHeaders});
   }
+
 }
